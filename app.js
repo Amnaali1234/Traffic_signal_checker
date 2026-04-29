476ea59b7a918aac00c92fc9d7d4a5c375e6eef9
Amnaali1234 <amnasheikh2255@gmail.com>
2026-04-30 01:17:13 +0500

    Traffic signal checker

---
+++ app.js
@@ -0,0 +1,13 @@
+function signalChecker() {
+let color = document.getElementById("colorInput").value.toLowerCase();
+let message = document.getElementById("message");
+if (color === "red") {
+    message.innerHTML = "Must Stop";
+} else if (color === "yellow") {
+    message.innerHTML = "Ready to move";
+} else if (color === "green") {
+    message.innerHTML = "Move now";
+} else {
+    message.innerHTML = "Invalid color";
+}
+}
